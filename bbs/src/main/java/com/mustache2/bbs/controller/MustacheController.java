bd7e7926be416c7387e65136250966e6a7ddf9a4
simhyunbo <hbo9347@naver.com>
2022-11-07 13:48:37 +0900

    mustache

--- bbs/src/main/java/com/mustache2/bbs/controller/MustacheController.java
+++ bbs/src/main/java/com/mustache2/bbs/controller/MustacheController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class MustacheController {
 
-    @GetMapping(value = "/hi/{id}")
-    public String mustacheCon(@PathVariable String id, Model model) {
-        model.addAttribute("username", "shb");// view에 값을 넘기겠
-        model.addAttribute("id", id);
-        return "greetings"; // greetings라는 이름의 view를 리턴
+    @GetMapping(value = {"/hi", "/hi/{id}"})
+    public String mustacheConWithPathVariable(Model model, @PathVariable(value = "id", required = false) String id) {
+        model.addAttribute("username", "kim");
+        if (id == null) {
+            model.addAttribute("id", "");
+        }
+        return "greetings";
     }
 }
